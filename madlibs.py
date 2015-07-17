fd2ae4c9ffa67dd4c069f8a879e0fc789e40e9e1
Hackbright Student <info@hackbrightacademy.com>
2015-07-16 18:07:05 -0700

    Progress on adding styling and extra functionality

--- madlibs.py
+++ madlibs.py
@@ -36,6 +36,14 @@ def show_game_form():
         return render_template("game.html", player=player)
     else:
         return render_template("goodbye.html", player=player)
+@app.route('/madlib')
+def show_madlib():
+    person = request.args.get('person')
+    color = request.args.get('color')
+    noun = request.args.get('noun')
+    adjective = request.args.get('adjective')
+
+    return render_template('madlib.html', person=person, color=color, noun=noun, adjective=adjective)
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
